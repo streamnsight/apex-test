226b1b2dd7d6103fdb1d85fe54cba08e9884e155
Emmanuel Leroy <eleroy@msn.com>
2023-03-17 10:02:05 -0700

    Initialize update/22.4

---
+++ terraform/provider.tf
@@ -0,0 +1,13 @@
+## Copyright © 2021, Oracle and/or its affiliates. 
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">= 4.85.0"
+      # https://registry.terraform.io/providers/hashicorp/oci/4.85.0
+    }
+  }
+}
